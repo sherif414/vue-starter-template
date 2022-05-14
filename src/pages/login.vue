fbf8136df21717fa0b975174406a8b5f29138e18
sherif414 <creazyhell@gmail.com>
2022-05-14 16:39:27 +0200

    enhancing the login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,39 +1,63 @@
 <template>
   <div
-    class="container mx-auto flex flex-col justify-center items-center h-screen"
+    class="bg-purple-100 justify-center px-[8vw] items-center flex h-screen text-gray-800"
   >
-    <form
-      @submit.prevent="login"
-      class="py-12 shadow bg-purple-100 flex flex-col justify-center items-center min-h-[80%] min-w-[50%]"
-    >
-      <h1 class="text-center font-bold mb-4">Login</h1>
-      <div class="mb-4">
-        <label class="block" for="username"> username </label>
-        <input
-          type="text"
-          name="username"
-          autocomplete="off"
-          id="username"
-          required
-          v-model="login_data.username"
-          class="px-2 focus:(outline-none) rounded"
-        />
+    <div class="flex rounded overflow-hidden shadow bg-white">
+      <div class="w-[50%] items-center flex flex-col">
+        <form
+          @submit.prevent="login"
+          class="h-full w-[50%] flex justify-center items-stretch flex-col"
+        >
+          <h1 class="text-3xl mb-4">Welcome back</h1>
+
+          <div class="mb-4">
+            <label class="block text-sm mb-1" for="username"> Username </label>
+            <input
+              type="text"
+              name="username"
+              autocomplete="off"
+              id="username"
+              required
+              placeholder="enter username"
+              v-model="login_data.username"
+              class="p-3 w-full focus:(outline-purple-200) text-sm border rounded"
+            />
+          </div>
+          <div class="mb-4">
+            <label class="block text-sm mb-1" for="password"> Password </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              required
+              placeholder="enter passowrd"
+              v-model="login_data.password"
+              class="p-3 w-full text-sm focus:(outline-purple-200) border rounded"
+            />
+          </div>
+          <div class="mb-4">
+            <input
+              type="checkbox"
+              class="text-purple-400 checked:text-purple-400 focus:(outline-purple-200)"
+              name="remember"
+              id="remember"
+            /><label class="text-sm ml-2" for="remember">remember me?</label>
+          </div>
+          <button
+            type="submut"
+            class="py-2 mb-2 bg-purple-400 hover:(bg-purple-600) focus:(bg-purple-600 outline-none) active:bg-purple-800 text-light-50 rounded"
+          >
+            login
+          </button>
+          <a href="#" class="text-xs text-purple-400 active:text-purple-600"
+            >Forgot password?</a
+          >
+        </form>
       </div>
-      <div class="mb-4">
-        <label class="block" for="password"> password </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          required
-          v-model="login_data.password"
-          class="px-2 focus:(outline-none) rounded"
-        />
+      <div class="w-[50%] h-full">
+        <img class="h-full" src="src/assets/login.jpg" />
       </div>
-      <button type="submut" class="px-4 py-2 bg-purple-300 rounded">
-        login
-      </button>
-    </form>
+    </div>
   </div>
 </template>
 <script setup>
