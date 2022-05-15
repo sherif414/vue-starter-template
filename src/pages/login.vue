7a300ce6ad154f2fb0c7d0c9df56464952966540
sherif414 <creazyhell@gmail.com>
2022-05-15 12:03:04 +0200

    inhanced login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,10 +3,10 @@
     class="bg-purple-100 justify-center px-[8vw] items-center flex h-screen text-gray-800"
   >
     <div class="flex rounded overflow-hidden shadow bg-white">
-      <div class="w-[50%] items-center flex flex-col">
+      <div class="w-[50%] flex flex-col">
         <form
           @submit.prevent="login"
-          class="h-full w-[50%] flex justify-center items-stretch flex-col"
+          class="h-full w-[50%] m-auto flex justify-center items-stretch flex-col"
         >
           <h1 class="text-3xl mb-4">Welcome back</h1>
 
@@ -20,8 +20,12 @@
               required
               placeholder="enter username"
               v-model="login_data.username"
-              class="p-3 w-full focus:(outline-purple-200) text-sm border rounded"
+              :class="errorClasses"
+              class="p-3 w-full text-sm border rounded"
             />
+            <p v-show="errors" class="text-red-600 text-xs m-1 truncate">
+              {{ errors }}
+            </p>
           </div>
           <div class="mb-4">
             <label class="block text-sm mb-1" for="password"> Password </label>
@@ -32,7 +36,8 @@
               required
               placeholder="enter passowrd"
               v-model="login_data.password"
-              class="p-3 w-full text-sm focus:(outline-purple-200) border rounded"
+              :class="errorClasses"
+              class="p-3 w-full text-sm border rounded"
             />
           </div>
           <div class="mb-4">
@@ -44,7 +49,7 @@
             /><label class="text-sm ml-2" for="remember">remember me?</label>
           </div>
           <button
-            type="submut"
+            type="submit"
             class="py-2 mb-2 bg-purple-400 hover:(bg-purple-600) focus:(bg-purple-600 outline-none) active:bg-purple-800 text-light-50 rounded"
           >
             login
@@ -62,16 +67,24 @@
 </template>
 <script setup>
 import axios from "axios";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useAuthStore } from "../stores/useAuth";
 
 const store = useAuthStore();
 const login_data = ref({});
+const res = ref();
+const errors = ref();
 
-const url = "http://127.0.0.1:8000/auth/jwt/create/";
+const errorClasses = computed(() => {
+  if (errors.value) {
+    return "border-red-500 focus:outline-red-500";
+  }
+  return "border-purple-200 focus:outline-purple-400";
+});
 async function login() {
+  errors.value = null;
   await axios({
-    url: url,
+    url: "http://127.0.0.1:8000/auth/jwt/create/",
     method: "post",
     headers: {
       "Content-Type": "application/json",
@@ -80,9 +93,20 @@ async function login() {
       username: login_data.value.username,
       password: login_data.value.password,
     },
-  }).then((response) => {
-    store.tokens.access = response.data.access;
-    store.tokens.refresh = response.data.refresh;
-  });
+  })
+    .then((response) => {
+      console.log(response);
+      if (response.status === 200) {
+        errors.value = false;
+      }
+      store.tokens.access = response.data.access;
+      store.tokens.refresh = response.data.refresh;
+      res.value = response;
+    })
+    .catch((err) => {
+      console.log(err.response);
+
+      errors.value = err.response.data.detail;
+    });
 }
 </script>
